eb44e8684030dd9b4f057678dd447934decb0d27
Fábio Junges Subtil <fabiosubtil@gmail.com>
2023-10-29 22:26:56 -0300

    instalação do GA4

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MTXRMLSH');</script>
+<!-- End Google Tag Manager -->
 <meta charset="UTF-8"/>
 <title>Cactus Café</title>
 
@@ -10,11 +17,15 @@
 
 </head>
 <body>
-	<div class="container">	
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MTXRMLSH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+	<div class="container">
 		<div class="bg-secondary opacity-50 p-3 text-white">
 			<h1>@yield('cabecalho')</h1>
-    	</div>  
-		@yield('conteudo')		
-	</div>	
-</body>	
+    	</div>
+		@yield('conteudo')
+	</div>
+</body>
 </html>
