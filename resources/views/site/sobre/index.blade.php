157ab83fd49e4105ffb24913f0be00db15579aac
Fábio Junges Subtil <fabiosubtil@gmail.com>
2023-11-02 16:16:26 -0300

    ajuste no texto sobre

--- resources/views/site/sobre/index.blade.php
+++ resources/views/site/sobre/index.blade.php
@@ -15,10 +15,10 @@
                     <h1 class="display-medium" style="margin-bottom: 0px;color:#FFFFFF;">O Cactus Café Poa é uma cafeteria localizada no tradicional bairro Auxiliadora em Porto Alegre.</h1>
                     <p style="color:#FFFFFF;">Reabre, após um hiato de alguns meses e sob nova direção, com a responsabilidade e o objetivo de somar-se às inúmeras opções de qualidade que o bairro oferece.</p>
                     <p style="color:#FFFFFF;margin-bottom:-20px;"><strong>Nossos valores:</strong></p>
-                    <p style="color:#FFFFFF;"><strong>Qualidade:</strong>  prezamos pela qualidade de nossos produtos, os quais fizemos com matérias primas selecionadas e com muito carinho.</p>
+                    <p style="color:#FFFFFF;"><strong>Qualidade:</strong>  prezamos pela qualidade de nossos produtos, os quais fazemos com matérias primas selecionadas e com muito carinho.</p>
                     <p style="color:#FFFFFF;"><strong>Transparência:</strong> nosso compromisso maior é com nossos clientes. Estes, fazem parte do nosso negócio.</p>
                     <p style="color:#FFFFFF;"><strong>Atendimento:</strong> procuramos oferecer um atendimento personalizado buscando se inserir no bairro e no cotidiano dos moradores e frequentadores.</p>
-                    <p style="color:#FFFFFF;"><strong>Distinção:</strong> acreditamops que a distinção no atendimento é a chave para prosperarmos, juntamento com toda a comunidade do bairro.</p>
+                    <p style="color:#FFFFFF;"><strong>Distinção:</strong> acreditamos que a distinção no atendimento é a chave para prosperarmos, juntamente com toda a comunidade do bairro.</p>
                 </section>
             </section>
             <section class="about__img__values"style="padding:5px 0px 5px 0px;">
